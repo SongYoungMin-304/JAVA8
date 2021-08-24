2ff785690318e8a256ec625bfa2d1cf32d249d8c
82108 <dudals789@naver.com>
2021-08-24 23:20:47 +0900

    1. 어노테이션 2. 병렬 정렬

---
+++ Java8/src/Chapter18/App.java
@@ -0,0 +1,21 @@
+package Chapter18;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Chicken
+public class App {
+	
+	public static void main(@Chicken String[] args) throws @Chicken RuntimeException {
+		List<@Chicken String> names = Arrays.asList("song");
+	}
+	
+	static class FeelsLikeChicken<@Chicken T>{
+		
+		public static <@Chicken C> void print (@Chicken C c) {
+			System.out.println(c);
+		}
+		
+	}
+	
+}
---
+++ Java8/src/Chapter20/App.java
@@ -0,0 +1,14 @@
+package Chapter20;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class App {
+	
+	// 가비지 컬렉터
+	public static void main(String[] args){
+		
+	}
+	
+}
